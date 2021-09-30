bbc6992794e7ecdeb3e425404aee99267bb9e8ce
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-01 00:40:45 +0300

    create project

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "Document";
+const screens = "LED";
+let screenPrice = 5;
+let rollback = 1;
+let fullPrice = screenPrice - rollback;
+const adaptive = true;
+alert("Are you ready?");
+console.log(fullPrice);
+console.log("hello");
