196ba4ec47142d96d4bffe29dd451792e7097f4f
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-02 00:55:17 +0300

    mas num multiplic

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const title = "Document";
-const screens = "LED";
-let screenPrice = 5;
-let rollback = 1;
-let fullPrice = screenPrice - rollback;
-const adaptive = true;
-alert("Are you ready?");
-console.log(fullPrice);
-console.log("hello");
+let fact = 1;
+let num = 266219;
+let str = String(num);
+let arr = str.split('');
+//console.log(arr); проверка
+for (let i = 0; i < arr.length; i++) {
+   fact = fact * arr[i];
+}
+console.log(fact);
+let three = fact ** 3;
+//console.log(three);проверка 
+let str2 = String(three);
+console.log(str2[0], str2[1]);
