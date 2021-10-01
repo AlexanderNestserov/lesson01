848aa46dc079b627b06ea0b7f38bfde0d10116f4
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-01 23:40:21 +0300

    les2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const title = "Document";
-const screens = "LED";
-let screenPrice = 5;
-let rollback = 1;
-let fullPrice = screenPrice - rollback;
-const adaptive = true;
-alert("Are you ready?");
-console.log(fullPrice);
-console.log("hello");
+//присвоение переменных
+let title = "Lesson02";
+let screens = "Simple, Complex, Interactive";
+let screenPrice = 500;
+let rollback = 40;
+let fullPrice = 50;
+let adaptive = true;
+console.log(typeof title);//тип title
+console.log(typeof fullPrice);//тип fullPrice
+console.log(typeof adaptive);//тип adaptive
+console.log(screens.length);//длина screens
+console.log('Cost of dev screens' + ' ' + screenPrice + '$');//вывод строки с screenPrice
+console.log('Cost of dev site' + ' ' + fullPrice + '$');//вывод строки с fullPrice
+screens = screens.toLowerCase();//перевод строки к нижнему регистру
+//console.log(screens);проверка
+console.log(screens.split(''));//массив всех символов строки переменной screens
+console.log(screens.split(', '));//массив всех слов стоки переменной screens
+console.log(fullPrice * (rollback / 100) + '%');
+
+
+
