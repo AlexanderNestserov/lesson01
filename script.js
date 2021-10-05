0e15f9f21a7c21057c5f89cf1f2662e66116fcd7
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-05 19:04:03 +0300

    reduce

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let fact = 1;
+/*let title = prompt("Your project name");
+//console.log(title);проверка присвоения
+let screens = prompt("Your screens type(Simple,")*/
+let fact;
 let num = 266219;
 let str = String(num);
 let arr = str.split('');
 //console.log(arr); проверка
-for (let i = 0; i < arr.length; i++) {
+/*for (let i = 0; i < arr.length; i++) {
    fact = fact * arr[i];
-}
+}*/
+fact = arr.reduce(function (num, sum) {
+   return num * sum;
+});
 console.log(fact);
 let three = fact ** 3;
 //console.log(three);проверка 
