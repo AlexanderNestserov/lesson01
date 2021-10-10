79d94939d44bfd8bd72d6123d1c01992e4b51c16
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-10 13:21:57 +0300

    L05dif2

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const title = "Document";
-const screens = "LED";
-let screenPrice = 5;
-let rollback = 1;
-let fullPrice = screenPrice - rollback;
-const adaptive = true;
-alert("Are you ready?");
-console.log(fullPrice);
-console.log("hello");
+'use strict';
+
+let arr = [];
+arr = ['1234', '2345', '3456', '4567', '5678', '6789', '7891'];
+let first = ['2', '4'];
+for (let v of arr) {
+   if (first.includes(String(v).charAt(0))) {
+      console.log(v);
+   }
+}
