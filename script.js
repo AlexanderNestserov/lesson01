1192fc964e0d2abc8d8b24f3d2cc35392543ea32
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-11 22:47:32 +0300

    L05dif2+

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
-let arr = [];
-arr = ['1234', '2345', '3456', '4567', '5678', '6789', '7891'];
-let first = ['2', '4'];
-for (let v of arr) {
-   if (first.includes(String(v).charAt(0))) {
-      console.log(v);
+let n = 100;
+
+use:
+for (let i = 2; i <= n; i++) {
+
+   for (let j = 2; j < i; j++) {
+      if (i % j == 0) { continue use; }
    }
+   console.log(i + " :Делители этого числа :1 и " + " " + i); // простое число
 }
