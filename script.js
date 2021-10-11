6cd758c9123e30f61150bba4b8a32503df22effd
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-11 20:35:00 +0300

    les07dif

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const title = "Document";
-const screens = "LED";
-let screenPrice = 5;
-let rollback = 1;
-let fullPrice = screenPrice - rollback;
-const adaptive = true;
-alert("Are you ready?");
-console.log(fullPrice);
-console.log("hello");
+'use strict';
+/*let week = ['Sunday', 'Monday', 'Tusday', 'Wednesday', 'Thusday', 'Friday', 'Satarday'];
+
+
+for (let i = 0; week.length > i; i++) {
+
+   let html = week[i];
+   if (i == 0) {
+      html = html.italics();
+   }
+   wee.innerHTML = html;
+   document.body.appendChild(wee);
+}
+let date = new Date();
+let a=alert(date.getDay());*/
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let wee = document.getElementByIdName('wee');
+wee.innerHTML = week.join('<br>');
+
+for (let i = 0; i < week.length; i++) {
+   wee.innerHTML = week[i];
+   if (week[i] == 'Понедельник') {
+     wee. 
+   }
+}
