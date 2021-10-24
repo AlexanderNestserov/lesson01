c2ba2f2636a120a179e313d1c2b859f276c2db2a
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-24 14:41:21 +0300

    lesso07d+

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 for (let i = 0, len = week.length; i < len; i++) {
+
    let html = week[i];
    let now = new Date();
-   let day = week[now.getDay() - 1];
+   let day = week[now.getDay()];
 
    if (html == day) {
       html = html.bold();
-      if (i > 4) {
+      if (week[i] == 'Saturday' || week[i] == 'Sunday') {
          html = html.italics();
       }
-   } else if (i > 4) {
+   } else if (week[i] == 'Saturday' || week[i] == 'Sunday') {
       html = html.italics();
    }
    const p = document.createElement('p');
    p.innerHTML = html;
    document.body.appendChild(p);
 }
-
-
-
-
-   // выходные
-
-
-
-
-
-/*let now = new Date();
-let day = week[now.getDay() - 1].bold();
-const p = document.createElement('p');
-p.innerHTML = week[now.getDay() - 1].bold();
-document.body.appendChild(p);*/
